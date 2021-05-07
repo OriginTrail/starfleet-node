712edbcf6b163c3ed0644468b034e7ec0c4e3376
Nikola Todorovic <nikolaztodorovic26@gmail.com>
2021-05-07 13:12:25 +0200

    Implemented changes from moonbeam related to multisignature

--- runtime/src/account.rs
+++ runtime/src/account.rs
@@ -2,7 +2,7 @@
 
 use codec::{Decode, Encode};
 use sha3::{Digest, Keccak256};
-use sp_core::{ecdsa, H160, H256};
+use sp_core::{ecdsa, H160, H256, ed25519, sr25519, RuntimeDebug};
 
 #[cfg(feature = "std")]
 pub use serde::{de::DeserializeOwned, Deserialize, Serialize};
@@ -17,28 +17,51 @@ impl From<ecdsa::Signature> for EthereumSignature {
     }
 }
 
-impl sp_runtime::traits::Verify for EthereumSignature {
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Eq, PartialEq, Clone, Encode, Decode, RuntimeDebug)]
+pub enum MultiSignature {
+    Ed25519(ed25519::Signature),
+    Sr25519(sr25519::Signature),
+    Ecdsa(EthereumSignature),
+}
+
+impl From<EthereumSignature> for MultiSignature {
+    fn from(x: EthereumSignature) -> Self {
+        MultiSignature::Ecdsa(x)
+    }
+}
+
+impl sp_runtime::traits::Verify for MultiSignature {
     type Signer = EthereumSigner;
-    fn verify<L: sp_runtime::traits::Lazy<[u8]>>(&self, mut msg: L, signer: &H160) -> bool {
-        let mut m = [0u8; 32];
-        m.copy_from_slice(Keccak256::digest(msg.get()).as_slice());
-        match sp_io::crypto::secp256k1_ecdsa_recover(self.0.as_ref(), &m) {
-            Ok(pubkey) => {
-                // TODO This conversion could use a comment. Why H256 first, then H160?
-                H160::from(H256::from_slice(Keccak256::digest(&pubkey).as_slice())) == *signer
-            }
-            Err(sp_io::EcdsaVerifyError::BadRS) => {
-                log::error!(target: "evm", "Error recovering: Incorrect value of R or S");
-                false
-            }
-            Err(sp_io::EcdsaVerifyError::BadV) => {
-                log::error!(target: "evm", "Error recovering: Incorrect value of V");
-                false
-            }
-            Err(sp_io::EcdsaVerifyError::BadSignature) => {
-                log::error!(target: "evm", "Error recovering: Invalid signature");
-                false
-            }
+    fn verify<L: sp_runtime::traits::Lazy<[u8]>>(
+        &self,
+        mut msg: L,
+        signer: &H160
+    ) -> bool {
+        match (self, signer) {
+            (MultiSignature::Ecdsa(ref sig), who) => {
+                let mut m = [0u8; 32];
+                m.copy_from_slice(Keccak256::digest(msg.get()).as_slice());
+                match sp_io::crypto::secp256k1_ecdsa_recover(sig.0.as_ref(), &m) {
+                    Ok(pubkey) => {
+                        H160::from(H256::from_slice(Keccak256::digest(&pubkey).as_slice())) ==
+                            *who
+                    },
+                    Err(sp_io::EcdsaVerifyError::BadRS) => {
+                        log::error!(target: "evm", "Error recovering: Incorrect value of R or S");
+                        false
+                    },
+                    Err(sp_io::EcdsaVerifyError::BadV) => {
+                        log::error!(target: "evm", "Error recovering: Incorrect value of V");
+                        false
+                    },
+                    Err(sp_io::EcdsaVerifyError::BadSignature) => {
+                        log::error!(target: "evm", "Error recovering: Invalid signature");
+                        false
+                    }
+                }
+            },
+            _ => false
         }
     }
 }
